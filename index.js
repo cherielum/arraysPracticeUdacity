ee1daf9c4b3285fb9d185a48c999437a692507c2
Cherie Lum <charismatic.cherie@gmail.com>
2018-02-04 15:17:42 -0500

    adding example

--- index.js
+++ index.js
@@ -4,3 +4,8 @@
 [3.14, "pi", 3, 1, 4, "Yum, I like pie!"]
 //with curley bracket sor no commas, it is invalid
 
+//Create an array called udaciFamily and add "Julia", "James", and your name to the array. Then, print the udaciFamily to the console using console.log.
+// your code goes here
+var udaciFamily = ["Julia", "James", "Cherie"]
+
+console.log(udaciFamily);
