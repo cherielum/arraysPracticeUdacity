7263f691029d79bd94b2d08b5f3da604327108fe
Cherie Lum <charismatic.cherie@gmail.com>
2018-02-13 12:40:32 -0500

    adding exercises

--- index.js
+++ index.js
@@ -166,3 +166,46 @@ const totals = bills.map( function (x) {
 });    
 
 console.log(totals );
+
+//arrays with rows and columns
+for (var row = 0; row <grid.length; row ++) {
+    for (var column = 0; column <grid[row].length; column++) {
+        console.log(grid[row][column]);
+    }
+}
+
+//doughnuts in each row
+
+var donutBox = [
+    ["glazed", "chocolate glazed", "cinnamon"],
+    ["powdered", "sprinkled", "glazed cruller"],
+    ["chocolate cruller", "Boston creme", "creme de leche"]
+  ];
+  
+  // here, donutBox.length refers to the number of rows of donuts
+  for (var row = 0; row < donutBox.length; row++) {
+    console.log(donutBox[row]);
+  }
+
+//shows each doughnut from row and column
+  for (var row = 0; row < donutBox.length; row++) {
+    // here, donutBox[row].length refers to the length of the donut array currently being looped over
+    for (var column = 0; column < donutBox[row].length; column++) {
+      console.log(donutBox[row][column]);
+    }
+  }
+
+  //Use a nested for loop to take the numbers array below and replace all of the values that are divisible by 2 (even numbers) with the string "even" and all other numbers with the string "odd".
+
+var numbers = [
+    [243, 12, 23, 12, 45, 45, 78, 66, 223, 3],
+    [34, 2, 1, 553, 23, 4, 66, 23, 4, 55],
+    [67, 56, 45, 553, 44, 55, 5, 428, 452, 3],
+    [12, 31, 55, 445, 79, 44, 674, 224, 4, 21],
+    [4, 2, 3, 52, 13, 51, 44, 1, 67, 5],
+    [5, 65, 4, 5, 5, 6, 5, 43, 23, 4424],
+    [74, 532, 6, 7, 35, 17, 89, 43, 43, 66],
+    [53, 6, 89, 10, 23, 52, 111, 44, 109, 80],
+    [67, 6, 53, 537, 2, 168, 16, 2, 1, 8],
+    [76, 7, 9, 6, 3, 73, 77, 100, 56, 100]
+];
