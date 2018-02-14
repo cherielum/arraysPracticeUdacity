eca7c906d7ab4da0393b26169fa81f394105b90e
Cherie Lum <charismatic.cherie@gmail.com>
2018-02-14 13:25:12 -0500

    adding exercise answer

--- index.js
+++ index.js
@@ -209,3 +209,24 @@ var numbers = [
     [67, 6, 53, 537, 2, 168, 16, 2, 1, 8],
     [76, 7, 9, 6, 3, 73, 77, 100, 56, 100]
 ];
+
+for (var row = 0; row < numbers.length; row++) {
+    for (var column = 0; column < numbers[row].length; column++){
+       
+        // if (numbers % 2 === 0) {
+        //     console.log("even");
+        // } else {
+        //     console.log("odd");
+        // }
+        
+        if (numbers[row][column] % 2 === 0) {
+            numbers[row][column]= "even";
+            }
+            else {
+                numbers[row][column]= "odd";
+            }
+       
+    console.log(numbers[row][column]);
+       
+    }
+}
